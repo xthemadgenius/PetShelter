6f79d195255de47de7c8de110f6f6546bfdd051c
xthemadgenius <javierjcjr@gmail.com>
2020-07-24 16:50:22 -0700

    initial commit

---
+++ client/build/precache-manifest.953bd670095f25486e77f07abc00cd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cb9b822ad5fc319184bee62d4707e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe3d50c45fc101df482",
+    "url": "/static/css/main.0b6a0f40.chunk.css"
+  },
+  {
+    "revision": "a90f2b66e517d89ad194",
+    "url": "/static/js/2.394a51ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.394a51ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe3d50c45fc101df482",
+    "url": "/static/js/main.4262f06a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
